6d6440c2fb1898eef3b0d33ac13a6e1cb8bf4658
Gabriel H <gh@macbook-pro.myfiosgateway.com>
2021-04-07 20:08:50 -0400

    Added logic to generate new pieces if placed outside toolbox

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -3,18 +3,37 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour {
     private bool isSelected = false;
+    private bool isPlaced = false;
+    private ToolBox toolBox;
+
+    private void Start() {
+        toolBox = FindObjectOfType<ToolBox>();
+    }
 
     private void Update() {
+        if(isPlaced) return;
+
         if(isSelected) {
-            if (Input.GetMouseButtonDown(0)) { if(CanPlace()) { Place(); } }
-            if (Input.GetMouseButtonDown(1) || Input.GetKeyDown(KeyCode.F)) { Flip(); }
-            if (Input.GetKeyDown(KeyCode.Z)) { Rotate(false); }
-            if (Input.GetKeyDown(KeyCode.X)) { Rotate(true); }
+            if (Input.GetMouseButtonDown(0)) {
+                Place();
+                return;
+            }
+            if (Input.GetMouseButtonDown(1) || Input.GetKeyDown(KeyCode.F)) {
+                Flip();
+            }
+            if (Input.GetKeyDown(KeyCode.Z)) {
+                Rotate(false);
+            }
+            if (Input.GetKeyDown(KeyCode.X)) {
+                Rotate(true);
+            }
 
             Vector3 _position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             transform.position = new Vector3(_position.x, _position.y, -6);
         } else {
-            if (Input.GetMouseButtonDown(0)) { Select(); }
+            if (Input.GetMouseButtonDown(0)) {
+                Select();
+            }
         }
     }
 
@@ -31,9 +50,15 @@ public class Piece : MonoBehaviour {
         return true;
     }
 
-    
+
     private void Place() {
         isSelected = false;
+        if(toolBox.IsInside(transform.position)) {
+            transform.position = toolBox.GetPositionForPiece(this.GetInstanceID());
+        } else {
+            toolBox.PopPiece(this.GetInstanceID());
+            isPlaced = true;
+        }
     }
 
     private void Flip() {
--- Assets/Scripts/ToolBox.cs
+++ Assets/Scripts/ToolBox.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ToolBox : MonoBehaviour {
     [SerializeField][Range(1,6)] int numPieces = 3;
+    [SerializeField][Range(0,255)] int boundClickCoyoteOffset = 0;
     
     Vector2 size;
     (Vector3,Piece)[] toolboxPieces;
@@ -29,7 +30,6 @@ public class ToolBox : MonoBehaviour {
         size = (Vector2)GetComponent<MeshRenderer>().bounds.size;
 
         string _seed = DateTime.Now.ToString();
-        Debug.Log($"Seed: {_seed}");
         rng = new System.Random(_seed.GetHashCode());
 
         toolboxPieces = new (Vector3, Piece)[numPieces];
@@ -48,7 +48,6 @@ public class ToolBox : MonoBehaviour {
     private Piece InstantiatePiece(Vector3 _position) {
         int _next = rng.Next(0, 100);
         PieceShape _shape = GetPieceShape(_next);
-        Debug.Log($"Shape: {_shape}");
         GameObject _prefab =  LoadPiecePrefab(_shape);
         return GameObject.Instantiate(_prefab, _position, Quaternion.identity).GetComponent<Piece>();
     }
@@ -73,5 +72,33 @@ public class ToolBox : MonoBehaviour {
         toolboxPieces[0] = (new Vector3(transform.position.x, transform.position.y + size.y / 3, -6), null);
         toolboxPieces[1] = (new Vector3(transform.position.x, transform.position.y, -6), null);
         toolboxPieces[2] = (new Vector3(transform.position.x, transform.position.y - size.y / 3, -6), null);
+    }
+
+    public Vector3 GetPositionForPiece(int _pieceID) {
+        foreach((Vector3, Piece) _piece in toolboxPieces) {
+            if(_pieceID == _piece.Item2.GetInstanceID()) {
+                Debug.Log("Found!");
+                return _piece.Item1;
+            }
+        }
+        Debug.LogError("Didn't find Position for piece for some reason.");
+        return Vector3.zero;
+    }
+
+    public void PopPiece(int _pieceID) {
+        for(int i = 0 ; i < toolboxPieces.Length ; i++) {
+            if(_pieceID == toolboxPieces[i].Item2.GetInstanceID()) {
+                Vector3 _pos = toolboxPieces[i].Item1;
+                toolboxPieces[i].Item2 = InstantiatePiece(_pos);
+            }
+        }
+    }
+
+    public bool IsInside(Vector3 _position) {
+        //TODO there's a bug with the boundaries here
+        return (_position.x > (transform.position.x - size.x - boundClickCoyoteOffset) &&
+                _position.x < (transform.position.x + size.x + boundClickCoyoteOffset) &&
+                _position.y > (transform.position.y - size.y - boundClickCoyoteOffset) &&
+                _position.y < (transform.position.y + size.y + boundClickCoyoteOffset));
     }
 }
